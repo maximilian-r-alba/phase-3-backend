3cc26a3488c0e2efa3d3b0ecee80a299ca8d63aa
Maximilian Alba <albam476@gmail.com>
2023-01-16 17:22:02 -0500

    Create Seeds for Books

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+50.times do
+    Book.create(
+        name: Faker::Book.title, 
+        genre: Faker::Book.genre, 
+        author: Faker::Book.author, 
+        rating: rand(1..5), 
+    )
 
+end
 
 
 
