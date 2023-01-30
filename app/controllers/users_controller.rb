c3b21c99bec36b8a74315bc48bba29bbb67d7998
Maximilian Rex Alba <albam476@gmail.com>
2023-01-29 23:51:59 -0500

    Add post route to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,13 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         user.to_json
     end
+
+    post "/users" do
+        user = User.create(user_params)
+        user.to_json
+    end
+
+    def user_params
+        {name: params[:name]}
+    end
 end
