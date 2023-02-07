b33f977e2bebb8779416f93d67d2cadfbbcf7ba8
maximilian-r-alba <100059063+maximilian-r-alba@users.noreply.github.com>
2023-02-07 17:01:41 +0000

    Test routes for alternative get requests in books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,22 +14,39 @@ class BooksController < ApplicationController
         sorted = Book.all.sort_by{|book| book.author}
         sorted.to_json
     end
+
+    # get '/books/author/:sort_method' do
+    #       case params[:sort_method]
+                    # when 'alphabetical'
+                    # sorted = Book.all.sort_by{|book| book.author}
+                    # sorted.to_json
+            # end
+
+    # end
+
     get '/books/genre/fiction' do
-        filtered = Book.all.select{|book| book.fiction?}
-        filtered.to_json
+        ficitonBooks = Book.all.select{|book| book.fiction?}
+        fictionBooks.to_json
     end
+
+    # get '/books/genre/fiction/:genre' do
+    #     ficitonBooks = Book.all.select{|book| book.fiction?}
+    #     filteredFiciton = fictionBooks.select{|book| book.genre == params[:genre]}
+    # end
+
     get '/books/genre/nonfiction' do
-        filtered = Book.all.select{|book| !book.fiction?}
-        filtered.to_json
+        nonfictionBooks = Book.all.select{|book| !book.fiction?}
+        nonfictionBooks.to_json
     end
 
     post '/books' do 
-        book = Book.create(book_params)
+        book = Book.create(book_params(params))
         book.to_json
     end
 
-    def book_params
+    def book_params(params)
         {title: params[:title], genre: params[:genre], author: params[:author], rating: params[:rating], cover_url: params[:cover_url]}
     end
+
   
 end
