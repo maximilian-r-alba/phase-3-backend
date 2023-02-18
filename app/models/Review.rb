b70d6d24a6597962eff247fc8dcab089916fd519
Maximilian Alba <albam476@gmail.com>
2023-02-18 15:50:56 -0500

    Fix calculation of rating

--- app/models/Review.rb
+++ app/models/Review.rb
@@ -1,18 +1,23 @@
 class Review < ActiveRecord::Base
     belongs_to :user
     belongs_to :book
-    after_create :calculate_rating
+    after_save :calculate_rating
+    after_destroy :calculate_rating
     has_many :comments
 
     private
         def calculate_rating
+
             book = self.book
 
-            reviews_sum = book.reviews.map{|review| review.rating}.sum.to_f + self.rating
+            reviews_sum = book.reviews.map{|review| review.rating}.sum.to_f
 
-            total_reviews = book.reviews.count + 1
+            total_reviews = book.reviews.count
             
             average_rating = (reviews_sum/total_reviews).round(2)
+            binding.pry
             book.update(rating:average_rating)
         end
+
+
 end
